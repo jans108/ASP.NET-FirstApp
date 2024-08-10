0690a3dd1210eca0539e0122d16ebdf2f49154a7
Maciej Broda <maciejbroda01@gmail.com>
2024-08-10 22:46:29 +0200

    Creating the Shopping Cart

--- BethanysPieShop/BethanysPieShop/Models/BethanysPieShopDbContext.cs
+++ BethanysPieShop/BethanysPieShop/Models/BethanysPieShopDbContext.cs
@@ -10,7 +10,7 @@ namespace BethanysPieShop.Models
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Pie> Pies { get; set; }
-
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
 
     }
 }
--- BethanysPieShop/BethanysPieShop/Program.cs
+++ BethanysPieShop/BethanysPieShop/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
 
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
@@ -16,6 +20,7 @@ builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 var app = builder.Build();
 
 app.UseStaticFiles();
+app.UseSession();
 
 if (app.Environment.IsDevelopment())
 {
