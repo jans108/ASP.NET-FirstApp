46c3c8dbf084f598e5875960fbed5f5d899208f5
Maciej Broda <maciejbroda01@gmail.com>
2024-08-16 22:42:27 +0200

    Recreating the Form Using Razor Pages

--- BethanysPieShop/BethanysPieShop/Program.cs
+++ BethanysPieShop/BethanysPieShop/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
     options.UseSqlServer(
@@ -29,5 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
 DbInitializer.Seed(app);
 app.Run();
