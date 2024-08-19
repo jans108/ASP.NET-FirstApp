4c9925a2f2909c74ab194098729b43d2c660d1bf
Maciej Broda <maciejbroda01@gmail.com>
2024-08-19 19:35:37 +0200

    Setting up the API

---
+++ BethanysPieShop/BethanysPieShop/Controllers/Api/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public SearchController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+
+        }
+    }
+}
--- BethanysPieShop/BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
+++ BethanysPieShop/BethanysPieShop/obj/Debug/net8.0/BethanysPieShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c3c8dbf084f598e5875960fbed5f5d899208f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a786dba10a8ca3d29d36ad04a5243c685843bb57")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
